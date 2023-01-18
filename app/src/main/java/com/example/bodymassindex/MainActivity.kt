51f7cd1bc59f7f32a6abbbb07aaef5b9ee97c59e
imraghavbnsal007 <raghavb025@gmail.com>
2023-01-18 17:36:59 +0200

    Body Mass Index

--- app/src/main/java/com/example/bodymassindex/MainActivity.kt
+++ app/src/main/java/com/example/bodymassindex/MainActivity.kt
@@ -3,13 +3,23 @@ package com.example.bodymassindex
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.bodymassindex.ui.theme.BodyMassIndexTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +32,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    Bmi()
                 }
             }
         }
@@ -30,14 +40,57 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Bmi()
+{
+    var heightInput by remember { mutableStateOf("") }
+    var WeightInput by remember { mutableStateOf("") }
+    val height = heightInput.toFloatOrNull() ?:0.0f
+    val weight = WeightInput.toIntOrNull() ?: 0
+    val bmi = if (weight > 0 && height > 0) weight / (height * height) else 0.0
+
+    Column() {
+        Text(
+            text = stringResource(R.string.Body_Mass_Index),
+            fontSize = 40.sp,
+            color = MaterialTheme.colors.primary,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 16.dp, bottom = 16.dp)
+        )
+        OutlinedTextField(
+            label = { Text(text = stringResource(R.string.Height))},
+            value = heightInput,
+            onValueChange = {heightInput = it.replace(',','.')},
+            singleLine = true,
+            keyboardOptions = KeyboardOptions (keyboardType = KeyboardType.Number),
+            modifier = Modifier.fillMaxWidth()
+
+        )
+        OutlinedTextField(
+            label = { Text(text = stringResource(R.string.Weight))},
+            value = WeightInput,
+            onValueChange = {WeightInput = it.replace(',','.')},
+            singleLine = true,
+            keyboardOptions = KeyboardOptions (keyboardType = KeyboardType.Number),
+            modifier = Modifier.fillMaxWidth()
+        )
+        Text(
+            modifier = Modifier.padding(8.dp),
+            text = stringResource(R.string.Result, String.format("%.2f", bmi).replace(',','.'))
+        )
+
+
+
+    }
 }
+
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     BodyMassIndexTheme {
-        Greeting("Android")
+        Bmi()
+
     }
 }
